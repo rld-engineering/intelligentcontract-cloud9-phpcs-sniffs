018a0d5b2c037b34af5b729e7f9b4ece0f1f5ab1
mike edwards <mike.edwards@happy-customer.co.uk>
2015-10-05 15:44:46 +0100

    add 2 statements test

--- tests/WhiteSpace/MultiLineStatementIndentTest.php
+++ tests/WhiteSpace/MultiLineStatementIndentTest.php
@@ -14,6 +14,7 @@ class WhiteSpace_MultiLineStatementIndentTest extends TestCase
     {
         return [
             'empty method' => [__DIR__ . '/_files/MultiLineStatementIndent/empty-method.php', []],
+            'two statements' => [__DIR__ . '/_files/MultiLineStatementIndent/two-statements.php', []],
             'method call arg' => [__DIR__ . '/_files/MultiLineStatementIndent/method-call-arg.php', []],
             'chained method call' => [__DIR__ . '/_files/MultiLineStatementIndent/chained-method-call.php', []],
             'chained method arg' => [__DIR__ . '/_files/MultiLineStatementIndent/chained-method-call-arg.php', []],
