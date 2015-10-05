8c38a1f480df737a0348da47f6cb724fa13fee62
mike edwards <mike.edwards@happy-customer.co.uk>
2015-10-05 17:19:44 +0100

    add tests

--- tests/WhiteSpace/ScopeIndentTest.php
+++ tests/WhiteSpace/ScopeIndentTest.php
@@ -13,6 +13,8 @@ class WhiteSpace_ScopeIndentTest extends TestCase
     public function sniffProvider()
     {
         return [
+            'switch statement' => [__DIR__ . '/_files/ScopeIndent/switch-statement.php', []],
+            'structure followed by statemment' => [__DIR__ . '/_files/ScopeIndent/structure-then-statement.php', []],
             'empty method' => [__DIR__ . '/_files/ScopeIndent/empty-method.php', []],
             'scope incorrect indent' => [
                 __DIR__ . '/_files/ScopeIndent/scope-incorrect-indent.php',
---
+++ tests/WhiteSpace/_files/ScopeIndent/switch-statement.php
@@ -0,0 +1,14 @@
+<?php
+
+class test
+{
+    
+    public function foo()
+    {
+        switch ($foo) {
+            case 'bar':
+                return true;
+        }
+    }
+    
+}
