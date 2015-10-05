ff942cecc3446f64e620743e84ce7479db34633b
mike edwards <mike.edwards@happy-customer.co.uk>
2015-10-05 21:32:45 +0100

    add test for 100% coverage

--- tests/ControlStructures/ClosingParenNewlineTest.php
+++ tests/ControlStructures/ClosingParenNewlineTest.php
@@ -13,6 +13,7 @@ class ControlStructures_ClosingParenNewlineTest extends TestCase
     public function sniffProvider()
     {
         return [
+            'incomplete for structure' => [__DIR__ . '/_files/ClosingParenNewline/incomplete-for.php', []],
             'single line structure' => [__DIR__ . '/_files/ClosingParenNewline/single-line-structure.php', []],
             'correct multi-line structure' => [__DIR__ . '/_files/ClosingParenNewline/multi-line-structure.php', []],
             'inccorrect multi-line structure' => [
